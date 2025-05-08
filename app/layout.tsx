6805283dbdb848e399177babcba3e12fd7d1e81c
euouae682 <jasonuzhao682@gmail.com>
2025-05-07 20:36:20 -0700

    updated website title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Wynn Assist",
+  title: "Wynncraft Build Assist",
   description: "a goofy web app by euouae",
 };
 
